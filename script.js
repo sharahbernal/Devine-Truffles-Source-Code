49681fd49a4529317c7d4ae0a15ff0f50de7f8d7
sharahbernal <97236525+sharahbernal@users.noreply.github.com>
2022-01-06 23:51:55 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,186 @@
+if (document.readyState == 'loading') {
+  document.addEventListener('DOMContentLoaded', ready)
+} else {
+  ready()
+}
+
+function openNav() {
+  document.getElementById("Navbar").style.width = "80%";
+  document.getElementById("Navbar").style.backgroundColor = "#cfcfcf";
+  document.getElementById("Navbar").style.visibility = "visible";
+}
+
+function closeNav() {
+  document.getElementById("Navbar").style.width = "0%";
+  document.getElementById("Navbar").style.backgroundColor = "#ffffff00";
+  document.getElementById("Navbar").style.visibility = "hidden";
+}
+
+//slideshow
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
+/*Add to the cart*/
+if (document.readyState == 'loading') {
+  document.addEventListener('DOMContentLoaded', ready)
+} else {
+  ready()
+}
+
+function ready() {
+  var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+  for (var i = 0; i < removeCartItemButtons.length; i++) {
+      var button = removeCartItemButtons[i]
+      button.addEventListener('click', removeCartItem)
+  }
+
+  var quantityInputs = document.getElementsByClassName('cart-quantity-input')
+  for (var i = 0; i < quantityInputs.length; i++) {
+      var input = quantityInputs[i]
+      input.addEventListener('change', quantityChanged)
+  }
+
+  var addToCartButtons = document.getElementsByClassName('shop-item-button')
+  for (var i = 0; i < addToCartButtons.length; i++) {
+      var button = addToCartButtons[i]
+      button.addEventListener('click', addToCartClicked)
+  }
+
+  document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+}
+
+function purchaseClicked() {
+  alert('Thank you for your purchase')
+  var cartItems = document.getElementsByClassName('cart-items')[0]
+  while (cartItems.hasChildNodes()) {
+      cartItems.removeChild(cartItems.firstChild)
+  }
+  updateCartTotal()
+}
+
+function removeCartItem(event) {
+  var buttonClicked = event.target
+  buttonClicked.parentElement.parentElement.remove()
+  updateCartTotal()
+}
+
+function quantityChanged(event) {
+  var input = event.target
+  if (isNaN(input.value) || input.value <= 0) {
+      input.value = 1
+  }
+  updateCartTotal()
+}
+
+function addToCartClicked(event) {
+  var button = event.target
+  var shopItem = button.parentElement
+  var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+  var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+  var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+  addItemToCart(title, price, imageSrc)
+  updateCartTotal()
+}
+
+function addItemToCart(title, price, imageSrc) {
+  var cartRow = document.createElement('div')
+  cartRow.classList.add('cart-row')
+  var cartItems = document.getElementsByClassName('cart-items')[0]
+  var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+  for (var i = 0; i < cartItemNames.length; i++) {
+      if (cartItemNames[i].innerText == title) {
+          alert('This item is already added to the cart')
+          return
+      }
+  }
+  var cartRowContents = `
+      <div class="cart-item cart-column">
+          <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
+          <span class="cart-item-title">${title}</span>
+      </div>
+      <span class="cart-price cart-column">${price}</span>
+      <div class="cart-quantity cart-column">
+          <input class="cart-quantity-input" type="number" value="1">
+          <button class="btn btn-danger" type="button">REMOVE</button>
+      </div>`
+  cartRow.innerHTML = cartRowContents
+  cartItems.append(cartRow)
+  cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
+  cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
+}
+
+function updateCartTotal() {
+  var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+  var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+  var total = 0
+  for (var i = 0; i < cartRows.length; i++) {
+      var cartRow = cartRows[i]
+      var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+      var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+      var price = parseFloat(priceElement.innerText.replace('P', ''))
+      var quantity = quantityElement.value
+      total = total + (price * quantity)
+  }
+  total = Math.round(total * 100) / 100
+  document.getElementsByClassName('cart-total-price')[0].innerText = 'P' + total
+}
+
+/* search bar */
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
+
+/*Email*/
+function sendEmail() {
+  var to = document.getElementById('email')
+  var subject = document.getElementById('subject')
+  var name = document.getElementById('name')
+  var message = document.getElementById('message')
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: "cainamargie.freelance@gmail.com",
+    Password: "ilovemrbeast",
+    To: 'cainamargie.freelance@gmail.com',
+    From: email,
+    Subject: subject,
+    Body: message,
+    Attachments: [
+      {
+        name: "File_Name_with_Extension",
+        path: "Full Path of the file"
+      }]
+  })
+    .then(function (message) {
+      alert("Mail has been sent successfully")
+      document.getElementById("myForm").reset();
+    });
+}
